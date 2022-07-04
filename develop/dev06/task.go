a415e907a52fdcaecd668e08f34663cd916d8fa3
gaaavr <ssg0808@yandex.ru>
2022-07-04 23:30:28 +0300

    add comments

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -88,11 +88,18 @@ func setFlags() (arguments, error) {
 	return args, nil
 }
 
+// Функция для определения и выбора колонок из аргумента команлной строки
 func colSelection(args arguments, rows []string) (res []int, err error) {
+	//  Объёявляем переменную для конвертации строкового значения числа в числовое
 	var numberCol int
+	// Разбиваем строку аргумента на части для конвертации в числовые значения
 	numbers := strings.Split(args.f, ",")
+	// В цикле проходим по числам в строковм представлении
 	for _, v := range numbers {
+		// Проверяем число на содержание знаков "-" для идентификации нескольких колонок подряд
 		if strings.Contains(v, "-") && len(v) > 1 && strings.Count(v, "-") == 1 {
+			// Если "-" находится перед числом, значит берем все числа от первого и до переданного,
+			// конвертируем их в числовое представление и добавляем в массив
 			if strings.Index(v, "-") == 0 {
 				numberCol, err = strconv.Atoi(strings.TrimPrefix(v, "-"))
 				if err != nil {
@@ -105,6 +112,9 @@ func colSelection(args arguments, rows []string) (res []int, err error) {
 					res = append(res, j)
 				}
 				continue
+				// Если "-" является последнием символом после числа,
+				// то берём все числа, пока последнее не станет равно номеру последней колонки,
+				// конвертируем их в числовое представление и добавляем в массив
 			} else if strings.Index(v, "-") == len(v)-1 {
 				numberCol, err = strconv.Atoi(strings.TrimSuffix(v, "-"))
 				if err != nil {
@@ -114,6 +124,9 @@ func colSelection(args arguments, rows []string) (res []int, err error) {
 					res = append(res, j)
 				}
 				continue
+				// Если "-" находится в промежутки между двумя числами,
+				// берём все числа в этом промежутке и конвертируем их числовое представление,
+				// а затем добавляем в массив
 			} else {
 				twoNumbers := strings.Split(v, "-")
 				colOne, err := strconv.Atoi(twoNumbers[0])
@@ -133,14 +146,18 @@ func colSelection(args arguments, rows []string) (res []int, err error) {
 				continue
 			}
 		}
+		// Конвертируем строковое представление в числовое, в случае ошибки возвращаем её
 		numberCol, err = strconv.Atoi(v)
 		if err != nil {
 			return nil, fmt.Errorf("введён неккоректный номер колонки")
 		}
+		// Проверяем, что переданное в аргументе число не превышает количество колонок строки
 		if numberCol > len(rows) {
 			return nil, fmt.Errorf("колонки с номером %d не существует", numberCol)
 		}
+		// Добавляем в массив полученное число
 		res = append(res, numberCol-1)
 	}
+	// Возвращаем массив номеров колонок, которые необходимо вывести
 	return res, nil
 }
