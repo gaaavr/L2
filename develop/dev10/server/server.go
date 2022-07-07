8b66adacc6b01422a0a08c8cc35f062fcec24f36
Гаврилин Сергей Сергеевич <s.gavrilin@usr.vipholding.local>
2022-07-07 17:46:47 +0300

    telnet in progress

---
+++ develop/dev10/server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+// Создаём сервер на 80 порту, работающий по протоколу tcp
+func main() {
+	// Слушаем соединение на 80 порту
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("server is running")
+	// В бесконечном цикле при получении соединения сообщаем клиенту о подключении
+	for {
+		con, err := lis.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		_, err = con.Write([]byte("Welcome to localhost:8080. I change the letter case to capital.\n"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Принимаем запросы в виде строк от клиента, преобразуем их в верхний регистр
+		// и возвращаем клиенту
+		for {
+			line, err := bufio.NewReader(con).ReadString('\n')
+			// В случае отключения клиента закрываем соединение и ждём соединения со следующим клиентом
+			if err != nil {
+				con.Close()
+				break
+			}
+			fmt.Println("original string:", line)
+			line = "capitalized string: " + strings.ToUpper(line)
+			_, err = con.Write([]byte(line))
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+
+	}
+}
---
+++ develop/dev10/task.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+/*
+
+Реализовать простейший telnet-клиент.
+
+Примеры вызовов:
+go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3s 1.1.1.1 123
+
+Требования:
+Программа должна подключаться к указанному хосту (ip или доменное имя + порт) по протоколу TCP.
+После подключения STDIN программы должен записываться в сокет, а данные полученные и сокета должны выводиться в STDOUT
+Опционально в программу можно передать таймаут на подключение к серверу (через аргумент --timeout, по умолчанию 10s)
+При нажатии Ctrl+D программа должна закрывать сокет и завершаться. Если сокет закрывается со стороны сервера,
+программа должна также завершаться. При подключении к несуществующему сервер,
+программа должна завершаться через timeout
+*/
+
+type join struct {
+	host, port string
+	timeout    time.Duration
+}
+
+func main() {
+	j, err := setFlags()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = j.client()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// Функция для  распарсивания аргументов командной строки
+func setFlags() (join, error) {
+	// Объявляем переменные для хранения значений флага
+	var (
+		h, p string
+		t    time.Duration
+	)
+	// Определяем флаги командной строки и парсим их в переменные
+	flag.DurationVar(&t, "timeout", 10*time.Second, "Таймаут на подключение к серверу")
+	flag.Parse()
+	if len(flag.Args()) != 2 {
+		return join{}, fmt.Errorf("invalid count of arguments")
+	}
+	h = flag.Arg(0)
+	p = flag.Arg(1)
+
+	// Инициализируем экземпляр структуры с аргументами командной строки и возвращаем их
+	args := join{
+		host:    h,
+		port:    p,
+		timeout: t,
+	}
+	return args, nil
+}
+
+// Метод для подключения к серверу
+func (j *join) client() error {
+	con, err := net.DialTimeout("tcp", j.host+":"+j.port, j.timeout)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		//закрываем соединение с сервером при выходе из программы
+		fmt.Println("Close connection")
+		con.Close()
+	}()
+	// Создаём канал для отслеживания сигналов от ОС
+	sign := make(chan os.Signal)
+	// При поступлении сигнала от ОС он поступит в канал, в этом случае соединение закроется
+	// и программа завершит работу
+	signal.Notify(sign, syscall.SIGQUIT)
+	go func() {
+		for {
+			select {
+			case <-sign:
+				con.Close()
+				return
+			default:
+			}
+		}
+	}()
+	// Читаем сообщение от сервера
+	reader, err := bufio.NewReader(con).ReadString('\n')
+	if err != nil {
+		return err
+	}
+	fmt.Print(reader)
+	scanner := bufio.NewScanner(os.Stdin)
+	// Со стандартоного ввода получаем строки и отправляем на сервер, который возвращает
+	// нам эти строки в верхнем регистре
+	for scanner.Scan() {
+		_, err := con.Write([]byte(scanner.Text() + "\n"))
+		if err != nil {
+			return err
+		}
+		line, err := bufio.NewReader(con).ReadString('\n')
+		if err != nil {
+			return err
+		}
+		os.Stdout.Write([]byte(line))
+	}
+	return nil
+}
